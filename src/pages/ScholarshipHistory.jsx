89f60bc042871d7a3bb8479ef418389a3e2b5662
saranbarua <saranbarua2357@gmail.com>
2025-06-29 16:14:34 +0600

    scholoarship page done

--- src/pages/ScholarshipHistory.jsx
+++ src/pages/ScholarshipHistory.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import chokkor from "../assets/no-data-found.svg";
 
-const ScholarshipHistory = () => {
-    return (
-        <div>
-            <h1>ScholarshipHistory</h1>
-        </div>
-    );
-};
-
-export default ScholarshipHistory;
+export default function ScholarshipHistory() {
+  return (
+    <div className="min-h-screen bg-white p-4 flex flex-col items-center justify-start">
+      {/* Advising Message */}
+      <div className="mt-10 text-center">
+        <img src={chokkor} alt="sync" className="w-24 h-24 mx-auto" />
+        <h2 className=" text-gray-700 text-xs mb-4">
+          Sorry, No scholarship & waiver data found!
+        </h2>
+      </div>
+    </div>
+  );
+}
